f05b550e06872d931d099008fc0bafacac13436e
tinywaves <dhzhme@gmail.com>
2024-04-16 00:55:09 +0800

    chore: eslint config file

---
+++ eslint.config.ts
@@ -0,0 +1,33 @@
+// @ts-expect-error missing types
+import styleMigrate from '@stylistic/eslint-plugin-migrate';
+import { dhzh } from './src';
+
+export default dhzh(
+  {
+    vue: true,
+    react: true,
+    solid: true,
+    svelte: true,
+    astro: true,
+    typescript: true,
+    formatters: true,
+  },
+  {
+    ignores: [],
+  },
+  {
+    files: ['src/**/*.ts'],
+    rules: {
+      'perfectionist/sort-objects': 'error',
+    },
+  },
+  {
+    files: ['src/configs/*.ts'],
+    plugins: {
+      'style-migrate': styleMigrate,
+    },
+    rules: {
+      'style-migrate/migrate': ['error', { namespaceTo: 'style' }],
+    },
+  },
+);
