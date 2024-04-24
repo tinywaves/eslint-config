3a650ade254640aa02919abebfe6f3c31e5d0d15
tinywaves <dhzhme@gmail.com>
2024-04-24 22:57:42 +0800

    feat: Update eslint version to 9 in update-package-json.ts

--- src/cli/stages/update-package-json.ts
+++ src/cli/stages/update-package-json.ts
@@ -19,16 +19,9 @@ export async function updatePackageJson(result: PromtResult) {
 
   pkg.devDependencies ??= {};
   pkg.devDependencies['@dhzh/eslint-config'] = `^${pkgJson.version}`;
-
-  // eslint-plugin-react currently does not support eslint9.
-  // reference: https://github.com/jsx-eslint/eslint-plugin-react/issues/3699
-  if (result.frameworks.includes('react')) {
-    pkg.devDependencies.eslint ??= '^8.57.0';
-  } else {
-    pkg.devDependencies.eslint ??= pkgJson.devDependencies.eslint
-      .replace('npm:eslint-ts-patch@', '')
-      .replace(/-\d+$/, '');
-  }
+  pkg.devDependencies.eslint ??= pkgJson.devDependencies.eslint
+    .replace('npm:eslint-ts-patch@', '')
+    .replace(/-\d+$/, '');
 
   const addedPackages: string[] = [];
 
