77252becb1b234a336d93e56371ca219be9092f6
tinywaves <dhzhme@gmail.com>
2024-06-12 20:14:40 +0800

    fix: only enable ts rules for ts files

--- src/configs/typescript.ts
+++ src/configs/typescript.ts
@@ -1,5 +1,5 @@
 import process from 'node:process';
-import { GLOB_SRC, GLOB_TS, GLOB_TSX } from '../globs';
+import { GLOB_TS, GLOB_TSX } from '../globs';
 import type { OptionsComponentExts, OptionsFiles, OptionsOverrides, OptionsTypeScriptParserOptions, OptionsTypeScriptWithTypes, TypedFlatConfigItem } from '../types';
 import { pluginAntfu } from '../plugins';
 import { interopDefault, renameRules, toArray } from '../utils';
@@ -14,7 +14,8 @@ export async function typescript(
   } = options;
 
   const files = options.files ?? [
-    GLOB_SRC,
+    GLOB_TS,
+    GLOB_TSX,
     ...componentExts.map((ext) => `**/*.${ext}`),
   ];
 
