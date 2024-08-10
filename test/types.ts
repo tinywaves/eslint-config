c39157c3b66c1dbdbfc2dbe0332fba9e1cb28993
dhzh <dhzhme@gmail.com>
2024-08-10 17:04:30 +0800

    chore: remove test

--- test/types.ts
+++
@@ -1,6 +0,0 @@
-import type { Linter } from 'eslint';
-import type { TypedFlatConfigItem } from '../src';
-
-// Make sure they are compatible
-((): Linter.FlatConfig => ({} as TypedFlatConfigItem))();
-((): TypedFlatConfigItem => ({} as Linter.FlatConfig))();
