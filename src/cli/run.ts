90f88e35b0fc11ed72ce3743c51ba828fbcd3c78
Lyle Zheng <dhzhme@gmail.com>
2025-03-10 03:35:32 +0800

    fix: object sort when prompts

--- src/cli/run.ts
+++ src/cli/run.ts
@@ -45,22 +45,14 @@ export async function run(options: CliRunOptions = {}): Promise<void> {
 
   if (!argSkipPrompt) {
     result = await p.group({
-      extra: ({ results }) => {
-        const isArgExtraValid = argExtra?.length
-          && !argExtra.filter((element) => !extra.includes(<ExtraLibrariesOption>element)).length;
-
-        if (!results.uncommittedConfirmed || isArgExtraValid) {
-          return;
+      uncommittedConfirmed: () => {
+        if (argSkipPrompt || isGitClean()) {
+          return Promise.resolve(true);
         }
 
-        const message = !isArgExtraValid && argExtra
-          ? `"${argExtra}" isn't a valid extra util. Please choose from below: `
-          : 'Select a extra utils:';
-
-        return p.multiselect<ExtraLibrariesOption>({
-          message: c.reset(message),
-          options: extraOptions,
-          required: false,
+        return p.confirm({
+          initialValue: false,
+          message: 'There are uncommitted changes in the current repository, are you sure to continue?',
         });
       },
       frameworks: ({ results }) => {
@@ -81,17 +73,24 @@ export async function run(options: CliRunOptions = {}): Promise<void> {
           required: false,
         });
       },
-      uncommittedConfirmed: () => {
-        if (argSkipPrompt || isGitClean()) {
-          return Promise.resolve(true);
+      extra: ({ results }) => {
+        const isArgExtraValid = argExtra?.length
+          && !argExtra.filter((element) => !extra.includes(<ExtraLibrariesOption>element)).length;
+
+        if (!results.uncommittedConfirmed || isArgExtraValid) {
+          return;
         }
 
-        return p.confirm({
-          initialValue: false,
-          message: 'There are uncommitted changes in the current repository, are you sure to continue?',
+        const message = !isArgExtraValid && argExtra
+          ? `"${argExtra}" isn't a valid extra util. Please choose from below: `
+          : 'Select a extra utils:';
+
+        return p.multiselect<ExtraLibrariesOption>({
+          message: c.reset(message),
+          options: extraOptions,
+          required: false,
         });
       },
-
       updateVscodeSettings: ({ results }) => {
         if (!results.uncommittedConfirmed) {
           return;
