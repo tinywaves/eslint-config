85d00cf189797eed1067d26d0932c93e0ff567b9
Lyle Zheng <dhzhme@gmail.com>
2025-04-24 22:23:46 +0800

    feat: add support for new import rules and plugins, including eslint-plugin-import-x and eslint-plugin-unused-imports

--- src/configs/imports.ts
+++ src/configs/imports.ts
@@ -1,37 +1,60 @@
-import { pluginAntfu, pluginImport } from '../plugins';
+import pluginImportX from 'eslint-plugin-import-x';
+import pluginUnusedImports from 'eslint-plugin-unused-imports';
+import pluginAntfu from 'eslint-plugin-antfu';
+import pluginSimpleImportSort from 'eslint-plugin-simple-import-sort';
+import { GLOB_SRC, RULE_PREFIX } from '../consts';
+import type { Linter, ESLint } from 'eslint';
+import type { IImportsConfigsOptions } from '../types';
 
-import type { OptionsStylistic, TypedFlatConfigItem } from '../types';
-
-export async function imports(options: OptionsStylistic = {}): Promise<TypedFlatConfigItem[]> {
-  const {
-    stylistic = true,
-  } = options;
+export function imports(options: IImportsConfigsOptions = {}): Linter.Config[] {
+  const { overrides = {}, closeOrder = true } = options;
 
   return [
     {
-      name: 'dhzh/imports/rules',
+      name: `${RULE_PREFIX}/imports/customize`,
+      files: GLOB_SRC,
       plugins: {
-        antfu: pluginAntfu,
-        import: pluginImport,
+        'antfu': pluginAntfu,
+        'import-x': pluginImportX as unknown as ESLint.Plugin,
+        'unused-imports': pluginUnusedImports,
+        'simple-import-sort': pluginSimpleImportSort,
       },
       rules: {
+        'sort-imports': 'off',
+        'no-unused-vars': 'off',
+        'no-duplicate-imports': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
         'antfu/import-dedupe': 'error',
         'antfu/no-import-dist': 'error',
         'antfu/no-import-node-modules-by-path': 'error',
-
-        'import/consistent-type-specifier-style': ['error', 'prefer-top-level'],
-        'import/first': 'error',
-        'import/no-duplicates': 'error',
-        'import/no-mutable-exports': 'error',
-        'import/no-named-default': 'error',
-        'import/no-self-import': 'error',
-        'import/no-webpack-loader-syntax': 'error',
-
-        ...stylistic
-          ? {
-              'import/newline-after-import': ['error', { count: 1 }],
-            }
-          : {},
+        'import-x/consistent-type-specifier-style': ['error', 'prefer-top-level'],
+        'import-x/first': 'error',
+        'import-x/no-duplicates': 'error',
+        'import-x/no-mutable-exports': 'error',
+        'import-x/no-named-default': 'error',
+        'import-x/no-self-import': 'error',
+        'import-x/no-webpack-loader-syntax': 'error',
+        'import-x/order': 'off',
+        'import-x/newline-after-import': [
+          'error',
+          {
+            count: 1,
+          },
+        ],
+        'unused-imports/no-unused-imports': 'warn',
+        'unused-imports/no-unused-vars': [
+          'error',
+          {
+            args: 'after-used',
+            argsIgnorePattern: '^_',
+            ignoreRestSiblings: true,
+            vars: 'all',
+            varsIgnorePattern: '^_',
+          },
+        ],
+        'simple-import-sort/imports': closeOrder ? 'off' : 'error',
+        'simple-import-sort/exports': closeOrder ? 'off' : 'error',
+        ...overrides,
       },
     },
   ];
