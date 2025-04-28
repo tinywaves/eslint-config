e28171dcaf3264796847797c904694ea8ee6ec97
Lyle Zheng <dhzhme@gmail.com>
2025-04-29 00:59:33 +0800

    fix: disable no-unsafe-call rule in TypeScript configuration

--- src/configs/typescript.ts
+++ src/configs/typescript.ts
@@ -57,6 +57,7 @@ export function typescript(options: ITypescriptConfigsOptions = {}): Linter.Conf
         '@typescript-eslint/ban-ts-comment': 'off',
         '@typescript-eslint/no-unsafe-assignment': 'off',
         '@typescript-eslint/no-unsafe-member-access': 'off',
+        '@typescript-eslint/no-unsafe-call': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/no-non-null-asserted-optional-chain': 'warn',
         '@typescript-eslint/await-thenable': 'error',
