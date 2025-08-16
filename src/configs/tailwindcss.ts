a043fb659f21f56fad1b8389dc7cf7e16a3ea0d7
tinywaves <dhzhme@gmail.com>
2025-08-17 06:27:18 +0800

    fix: tailwindcss configs only effect under tailwindcss package exists

--- src/configs/tailwindcss.ts
+++ src/configs/tailwindcss.ts
@@ -1,4 +1,5 @@
 import pluginTailwindcss from 'eslint-plugin-tailwindcss';
+import { isPackageExists } from 'local-pkg';
 import { RULE_PREFIX, GLOB_SRC } from '../consts';
 import type { Linter } from 'eslint';
 import type { ITailwindcssConfigsOptions } from '../types';
@@ -6,19 +7,21 @@ import type { ITailwindcssConfigsOptions } from '../types';
 export function tailwindcss(options: ITailwindcssConfigsOptions = {}): Linter.Config[] {
   const { overrides = {} } = options;
 
-  return [
-    ...pluginTailwindcss.configs['flat/recommended'].map((item) => ({
-      ...item,
-      name: `${RULE_PREFIX}/tailwindcss/shared/${item.name?.replace('tailwindcss:', '')}`,
-      files: GLOB_SRC,
-    })) as Linter.Config[],
-    {
-      name: `${RULE_PREFIX}/tailwindcss/customize`,
-      files: GLOB_SRC,
-      rules: {
-        'tailwindcss/no-custom-classname': 'off',
-        ...overrides,
-      },
-    },
-  ];
+  return isPackageExists('tailwindcss')
+    ? [
+        ...pluginTailwindcss.configs['flat/recommended'].map((item) => ({
+          ...item,
+          name: `${RULE_PREFIX}/tailwindcss/shared/${item.name?.replace('tailwindcss:', '')}`,
+          files: GLOB_SRC,
+        })) as Linter.Config[],
+        {
+          name: `${RULE_PREFIX}/tailwindcss/customize`,
+          files: GLOB_SRC,
+          rules: {
+            'tailwindcss/no-custom-classname': 'off',
+            ...overrides,
+          },
+        },
+      ]
+    : [];
 }
